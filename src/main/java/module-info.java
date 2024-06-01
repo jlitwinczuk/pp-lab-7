da5468f5ca8f31331d4396565d4e4b05d70668f6
jlitwinczuk <166211283+jlitwinczuk@users.noreply.github.com>
2024-06-01 12:40:01 +0200

    Add files via upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.zad3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.zad3 to javafx.fxml;
+    exports com.example.zad3;
+}
